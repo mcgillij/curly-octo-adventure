fdb9dc630d5b4a6a7375e55949ae0090b41f5255
pascal <pascal_larocque@hotmail.com>
2015-11-01 15:10:41 -0400

    modified game.py with test message

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ This is an empty project template that facilitates the scene paradigm.
 import sys
 import pygame as pg
 from data.main import main
-
+print "hi"
 
 if __name__ == '__main__':
     main()
